1d7c7a2633d3165af3a16a35800fba95ab88bf83
唐茵 <18373394@buaa.edu.cn>
2020-11-05 16:32:47 +0800

    add test

--- cookbook3/test/controllers/recipes_controller_test.rb
+++ cookbook3/test/controllers/recipes_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @recipe = recipes(:one)
+    @recipe = recipes(:one).
+    @category = category(:one)
   end
 
   test "should get index" do
